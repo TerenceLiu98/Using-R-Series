8aa3e4f99f1e6116778da07dd5bbb2fcd1d2e9e0
Terence Lau <terenceliu1012@gmail.com>
2018-12-08 02:50:17 +0800

    update

--- R-Project/Box-TidWell.R
+++ R-Project/Box-TidWell.R
@@ -1,40 +1,25 @@
-i <- NULL
-for ( i in 1:length(cleanbeijing_combin$timestamp)){
-    if(beijing.data$month[i] == 3){
-        beijing.data$season[i] <- "Spring"
-    }
-    if(beijing.data$month[i] == 4){
-        beijing.data$season[i] <- "Spring"
-    }
-    if(beijing.data$month[i] == 5){
-        beijing.data$season[i] <- "Spring"
-    }
-    if(beijing.data$month[i] == 6){
-        beijing.data$season[i] <- "Summer"
-    }
-    if(beijing.data$month[i] == 7){
-        beijing.data$season[i] <- "Summer"
-    }
-    if(beijing.data$month[i] == 8){
-        beijing.data$season[i] <- "Summer"
-    }
-    if(beijing.data$month[i] == 9){
-        beijing.data$season[i] <- "Autumn"
-    }
-    if(beijing.data$month[i] == 10){
-        beijing.data$season[i] <- "Autumn"
-    }
-    if(beijing.data$month[i] == 11){
-        beijing.data$season[i] <- "Autumn"
-    }
-    if(beijing.data$month[i] == 12){
-        beijing.data$season[i] <- "Winter"
-    }
-    if(beijing.data$month[i] == 1){
-        beijing.data$season[i] <- "Winter"
-    }
-    if(beijing.data$month[i] == 2){
-        beijing.data$season[i] <- "Winter"
-    }
+boxtidwell <- function(y,x,tol = .001, max.iter = 25){
+  alpha <- 1
+  iter <- 1
+  repeat{
+    y <- as.matrix(y)
+    x <- as.matrix(x)
+    if (any(x <= 0)) stop("the variables to be transformed must have only positive values")
+    var.names <- if(is.null(colnames(x))) seq(length.out=ncol(x)) else colnames(x)
+    # Retrieve or set the row or column names of a matrix-like object. use clonams to get the length of x
+    #‘is.null’ returns ‘TRUE’ if its argument's value is ‘NULL’ and
+    # ‘FALSE’ otherwise.
+    k.x <- length(var.names) # the number of the independent varible
+    x.log.x <- x*log(x) # alpha
+    alpha <- rep(1, length(var.names))
+    mod.1 <- get.Beta.hat(y ,  x)
+    mod.2 <- get.Beta.hat(y , cbind(x,x.log.x)) # new model
+    alpha <- (mod.2$coefficients[-c(1,2)] / mod.1$coefficients) * alpha
+    iter <- iter + 1
+    if(alpha <= tol || iter >= max.iter){
+    x <- cbind(x,x.log.x)
+      break
+  }
+ }
+   return(list("iter" = iter, "alpha" = alpha))
 }
-head(cleanbeijing_combin)
---
+++ R-Project/lm_code.R
@@ -0,0 +1,29 @@
+get.Beta.hat <- function(y,x){
+  y <- as.matrix(y)
+  x <- cbind(constant = 1, as.matrix(x))
+  beta.hat <- solve(t(x)%*%x,t(x)%*%y) # (x'x)^-1 * (x'y)
+
+  # Compute standard errors
+  s2 <- sum((y - x%*%beta.hat)^2)/(nrow(x) - ncol(x))
+  VCV <- s2*solve(t(x)%*%x)
+  SE <- sqrt(diag(VCV))
+
+  # Compute t-values
+  t <- beta.hat/SE
+
+  # Compute p-values
+  p <- 2*pt(abs(t),nrow(x) - ncol(x), lower.tail = FALSE)
+
+  # Compute adjusted R-squared
+  y_hat <- x%*%beta.hat
+  SSr <- sum((y - y_hat)^2)
+  SSt <- sum((y - mean(y))^2)
+  R2 <- 1 - (SSr/SSt)
+  adj.R2 <- 1 - ((1 - R2)*(nrow(x) - 1))/(nrow(x) - ncol(x[,-1]) - 1)
+
+
+  Table <- as.data.frame(round(cbind(beta.hat,SE,t,p, R2), digits = 4))
+  names(Table)[1:5] <- c("Estimate","Standard Error","t-value","p-value", "R^2")
+  paste("adj R square is ", adj.R2)
+  return(list("Table" = Table, "coefficients" = beta.hat))
+}
