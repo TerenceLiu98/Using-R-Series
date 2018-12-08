6c56387e599b75c104c575489d1a765cfdc12b27
Terence Lau <terenceliu1012@gmail.com>
2018-12-09 00:36:44 +0800

    udpate

--- R-Project/Box-TidWell.R
+++ R-Project/Box-TidWell.R
@@ -1,26 +1,29 @@
-boxtidwell <- function(y,x,tol = .001, max.iter = 25){
+boxtidwell <- function(y,x,tol = .01, max.iter = 25){
   iter <- 1
-  alpha <- rep(1, length(var.names))
   y <- as.matrix(y)
   x <- as.matrix(x)
-  
+  alpha <- rep(1, ncol(x))
+
+
   repeat{
     y <- as.matrix(y)
     x <- as.matrix(x)
-    if (any(x <= 0)) stop("the variables to be transformed must have only positive values")
-    var.names <- if(is.null(colnames(x))) seq(length.out=ncol(x)) else colnames(x)
+    # if (any(x <= 0)) stop("the variables to be transformed must have only positive values")
+    # var.names <- if(is.null(colnames(x))) seq(length.out=ncol(x)) else colnames(x)
     # Retrieve or set the row or column names of a matrix-like object. use clonams to get the length of x
     #‘is.null’ returns ‘TRUE’ if its argument's value is ‘NULL’ and
     # ‘FALSE’ otherwise.
-    k.x <- length(var.names) # the number of the independent varible
+    # k.x <- length(var.names) # the number of the independent varible
     x.log.x <- x*log(x) # alpha
-    
+    # which.coefs <- 2:(1 + k.x)
     mod.1 <- get.Beta.hat(y ,  x)
     mod.2 <- get.Beta.hat(y , cbind(x,x.log.x)) # new model
-    alpha <- (mod.2$coefficients[-c(1,2)] / mod.1$coefficients) * alpha
+    alpha <- (coefficients(mod.2)[ncol(x):ncol(x.log.x)]/coefficients(mod.1)[1:ncol(x)] + 1) * alpha
+    print(iter)
+    print(alpha)
     iter <- iter + 1
-    if(alpha <= tol || iter >= max.iter){
     x <- x^alpha
+    if(alpha <= tol || iter >= max.iter){
       break
     }
   }
--- R-Project/lm_code.R
+++ R-Project/lm_code.R
@@ -3,8 +3,12 @@ get.Beta.hat <- function(y,x){
   x <- cbind(constant = 1, as.matrix(x))
   beta.hat <- solve(t(x)%*%x,t(x)%*%y) # (x'x)^-1 * (x'y)
 
+  for(i in length(beta.hat)){
+    names(beta.hat) <- c("intercept", [i])
+  }
+
   # Compute standard errors
-  s2 <- sum((y - x%*%beta.hat)^2)/(nrow(x) - ncol(x))
+  s2 <- sum((y - x%*% beta.hat)^2)/(nrow(x) - ncol(x))
   VCV <- s2*solve(t(x)%*%x)
   SE <- sqrt(diag(VCV))
 
@@ -21,9 +25,9 @@ get.Beta.hat <- function(y,x){
   R2 <- 1 - (SSr/SSt)
   adj.R2 <- 1 - ((1 - R2)*(nrow(x) - 1))/(nrow(x) - ncol(x[,-1]) - 1)
 
-
+  beta.hat <- as.numeric(beta.hat)
   Table <- as.data.frame(round(cbind(beta.hat,SE,t,p, R2), digits = 4))
   names(Table)[1:5] <- c("Estimate","Standard Error","t-value","p-value", "R^2")
   paste("adj R square is ", adj.R2)
-  return(list("Table" = Table, "coefficients" = beta.hat))
+  return(list("Table" = Table, "coefficients" = beta.hat,"y_hat" = y_hat))
 }
