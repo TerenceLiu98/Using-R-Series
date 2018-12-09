846d32d4e4241267d83c4c6e17dec9506c32726e
Terence Lau <terenceliu1012@gmail.com>
2018-12-10 00:15:55 +0800

    update

--- R-Project/code/Box-TidWell.R
+++ R-Project/code/Box-TidWell.R
@@ -10,21 +10,12 @@ boxtidwell <- function(y,x,tol = .001, max.iter = 25){
     x <- as.matrix(x)
 
 
-<<<<<<< HEAD:R-Project/code/Box-TidWell.R
     w.log.w <- w*log(w) # alpha
     mod.1 <- get.Beta.hat(y ,  w)
     mod.2 <- get.Beta.hat(y , cbind(w,w.log.w)) # new model
     alpha <- (mod.2$coefficients[-c(1,2)] / mod.1$coefficients[-1] + 1) * alpha
     print(iter)
     print(alpha)
-=======
-    x.log.x <- x*log(x) # alpha
-    mod.1 <- get.Beta.hat(y ,  x)
-    mod.2 <- get.Beta.hat(y , cbind(x,x.log.x)) # new model
-    storage <- NULL
-    storage[iter] <- alpha <- (mod.2$coefficients[-c(1,2)] / mod.1$coefficients[-1] + 1) * alpha
-
->>>>>>> fde1025dcc01101ee71a80a9c9f0278c4325228c:R-Project/Box-TidWell.R
     if(alpha <= tol || iter >= 4){
       break
     }
@@ -40,9 +31,6 @@ boxtidwell <- function(y,x,tol = .001, max.iter = 25){
 	}
 	}
   }
-<<<<<<< HEAD:R-Project/code/Box-TidWell.R
-  return(list("iter" = iter, "alpha" = alpha,"R^2 after transformation" = mod.2$R2))
-=======
-  return(list("iter" = iter - 1, "alpha" = alpha,"alpha all" = storage))
->>>>>>> fde1025dcc01101ee71a80a9c9f0278c4325228c:R-Project/Box-TidWell.R
+  return(list("iter" = iter, "alpha" = alpha))
 }
+
